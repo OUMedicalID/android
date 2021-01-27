77bdba277fb8e7a095b5a8c6462cfa705075ad7c
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2021-01-27 01:56:43 -0500

    Add AES Encryption class

---
+++ app/src/main/java/com/lexycon/hostcardemulation/AESEncryption.java
@@ -0,0 +1,53 @@
+package com.lexycon.hostcardemulation;
+// We can implement regular java and call it from the android classes such as Home or MainActivity for example.
+// We could have chosen to implement import java.util.Base64 but we will implement the android version as it has more benefits.
+
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+//import java.util.Base64;
+import android.util.Base64;
+
+
+public class AESEncryption {
+
+    private static final String key = "81dc9bdb52d04dc20036dbd8313ed055";
+    private static final String initVector = "0000000000000000";
+
+
+    public static String encrypt(String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            return  Base64.encodeToString(encrypted, Base64.DEFAULT);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] original = cipher.doFinal(Base64.decode(encrypted, Base64.DEFAULT));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+}
+
