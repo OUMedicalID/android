866fe049c74645d9cd1b38e1c4d73a9cb72d2a2e
Yousif Hanani <ythanani@oakland.edu>
2021-01-31 20:37:50 -0500

    fixed password page

--- app/src/main/java/edu/oaklandstudent/medicalid/password.java
+++ app/src/main/java/edu/oaklandstudent/medicalid/password.java
@@ -1,37 +1,56 @@
 package edu.oaklandstudent.medicalid;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.thejuki.kformmaster.helper.FormBuildHelper;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import edu.oaklandstudent.medicalid.R;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
+
+import com.thejuki.kformmaster.item.ListItem;
 import com.thejuki.kformmaster.listener.OnFormElementValueChangedListener;
 import com.thejuki.kformmaster.model.BaseFormElement;
 import com.thejuki.kformmaster.model.FormButtonElement;
 import com.thejuki.kformmaster.model.FormHeader;
+import com.thejuki.kformmaster.model.FormLabelElement;
+import com.thejuki.kformmaster.model.FormPasswordEditTextElement;
+import com.thejuki.kformmaster.model.FormPhoneEditTextElement;
+import com.thejuki.kformmaster.model.FormPickerDateElement;
+import com.thejuki.kformmaster.model.FormPickerDropDownElement;
+import com.thejuki.kformmaster.model.FormSegmentedElement;
+import com.thejuki.kformmaster.model.FormSingleLineEditTextElement;
+import com.thejuki.kformmaster.helper.FormBuildHelper;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function2;
+public class password extends AppCompatActivity{
 
-public class password extends Fragment {
+    final FormPasswordEditTextElement password = new FormPasswordEditTextElement(1);
+    final FormPasswordEditTextElement confPassword = new FormPasswordEditTextElement(1);
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.informationcontainer);
 
 
-        View RootView = inflater.inflate(R.layout.password, container, false);
-        RecyclerView mRecyclerView = (RecyclerView) RootView.findViewById(R.id.passwordView);
+        // initialize variables
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerViewic);
 
         FormBuildHelper formBuilder = new FormBuildHelper(new OnFormElementValueChangedListener() {
             @Override
@@ -39,21 +58,75 @@ public class password extends Fragment {
                 Log.i("FirstForm", "Something in the form was changed.");
             }
         }, mRecyclerView);
+
         formBuilder.attachRecyclerView(mRecyclerView);
+
         // Declare our array list of elements
         List<BaseFormElement<?>> elements = new ArrayList<>();
-        // declare first section (header +  various elements)
-        FormHeader header = new FormHeader("Choose Information To Edit");
 
 
+        // First contact
+        final FormHeader header1 = new FormHeader("Emergency Contact 1");
 
-        elements.add(header);
+
+        password.setValue(password.getValueAsString());
+
+        FormLabelElement label = new FormLabelElement(1);
+        label.setTitle("Password");
+
+
+        confPassword.setValue(confPassword.getValueAsString());
+
+        FormLabelElement label2 = new FormLabelElement(1);
+        label2.setTitle("Confirm Password");
+
+        FormButtonElement save = new FormButtonElement(4);
+        save.setValue("Save Password");
+            save.getValueObservers().add(new Function2<String, BaseFormElement<String>, Unit>() {
+                @Override
+                public Unit invoke(String newValue, BaseFormElement<String> element) {
+                    if(password.getValueAsString().equals(confPassword.getValueAsString())) {
+                        SharedPreferences prefs = getSharedPreferences("edu.oaklandstudent.medicalid", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+
+                        editor.putString("password", AESEncryption.encrypt(password.getValue()));
+                        editor.putString("confPassword", AESEncryption.encrypt(confPassword.getValue()));
+
+                        editor.apply();
+                        Log.v("saved","Password saved");
+                    }
+                    else{
+                        Log.v("error","Passwords don't match");
+                    }
+                    Log.v("Main", "The button was pressed.");
+                    return Unit.INSTANCE;
+                }
+            });
+
+
+        elements.add(header1);
+
+        elements.add(label);
+        elements.add(password);
+        elements.add(label2);
+        elements.add(confPassword);
+        elements.add(save);
 
 
         formBuilder.addFormElements(elements);
+        // formBuilder.
+
+        SharedPreferences prefs = getSharedPreferences("edu.oaklandstudent.medicalid", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        password.setValue(AESEncryption.decrypt(prefs.getString("password", null)));
+        confPassword.setValue(AESEncryption.decrypt(prefs.getString("confPassword", null)));
 
 
-
-        return RootView;
     }
+
+    public String getPassword(){
+        return password.getValueAsString();
+    }
+
+
 }
